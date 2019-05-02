d5d1912bf1ae6f82b449c849659e67fefcf1a81d
UndoUreche <50205200+UndoUreche@users.noreply.github.com>
2019-05-02 16:22:06 +0300

    Add files via upload

---
+++ DSATeamWork-master/cmake-build-debug/CMakeFiles/DSATeamWork.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSATeamWork.dir/Heap.cpp.obj"
+  "CMakeFiles/DSATeamWork.dir/Tests.cpp.obj"
+  "CMakeFiles/DSATeamWork.dir/main.cpp.obj"
+  "DSATeamWork.pdb"
+  "DSATeamWork.exe"
+  "DSATeamWork.exe.manifest"
+  "libDSATeamWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSATeamWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
